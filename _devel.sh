2010fd1bec9c29c81385c88c335be9a463932eab
Armand J Komenan  (Kassidy) <armand_komenan@yahoo.com>
2018-04-03 18:03:03 -0400

    Update _devel.sh

--- _devel.sh
+++ _devel.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 mkdir -p roles
-git clone https://github.com/gbraad/ansible-role-docker.git roles/ArmandK.docker
-git clone https://github.com/ArmandK/ansible-role-docker-registry.git roles/ArmandK.docker-registry
-git clone https://github.com/ArmandK/ansible-role-kubernetes-master.git roles/ArmandK.kubernetes-master
-git clone https://github.com/ArmandK/ansible-role-kubernetes-node.git roles/ArmandK.kubernetes-node
-git clone https://github.com/ArmandK/ansible-role-kubernetes-client.git roles/ArmandK.kubernetes-client
-
-
+git clone https://github.com/armandk/ansible-role-docker.git roles/armandk.docker
+git clone https://github.com/armandk/ansible-role-docker-registry.git roles/armandk.docker-registry
+git clone https://github.com/armandk/ansible-role-kubernetes-master.git roles/armandk.kubernetes-master
+git clone https://github.com/armandk/ansible-role-kubernetes-node.git roles/armandk.kubernetes-node
+git clone https://github.com/armandk/ansible-role-kubernetes-client.git roles/armandk.kubernetes-client
